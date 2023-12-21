f2d209e8be97ba61cd3ced0c8d7b9e3157aee092
tausifc2004 <tac46@njit.edu>
2023-12-21 17:43:43 -0500

    added terrain, player controller, wild animal enemy AI

---
+++ Assets/Scripts/Raycasting.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+    public GameObject torch;
+    private GameObject torch2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = -Vector3.one;
+
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                clickPosition = hit.point;
+                torch2 = Instantiate(torch);
+                torch2.transform.position = clickPosition;
+                Destroy(torch2, 5);
+
+            }
+        }
+        
+    }
+}
