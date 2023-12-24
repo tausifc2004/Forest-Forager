05fcfa136ed64848aa62773c35fd125f53410fc0
tausifc2004 <tac46@njit.edu>
2023-12-23 22:55:36 -0500

    added lost screen when player HP is zero and added win screen when player reaches the house

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gameplay : MonoBehaviour
 {
@@ -9,12 +10,22 @@ public class Gameplay : MonoBehaviour
     public TextMeshProUGUI keyCountText;
     [SerializeField] private int playerHP = 100;
     public TextMeshProUGUI playerHPText;
-    public GameObject door;
+
+    public GameObject doorCollider;
+    public MeshRenderer doorRenderer;
+
+    public GameObject timerText;
+    public GameObject lostText;
+    public GameObject winText;
+    public GameObject retryText;
+
+    [SerializeField] private GameObject gameOverMenu;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        door = GameObject.FindGameObjectWithTag("Door");
+        doorCollider = GameObject.FindGameObjectWithTag("Door");
         SetCountText();
         SetHPText();
     }
@@ -24,7 +35,8 @@ public class Gameplay : MonoBehaviour
         keyCountText.text = "Keys: " + keyCount.ToString() + "/5";
         if (keyCount == 5)
         {
-            door.SetActive(false);
+            doorCollider.GetComponent<Collider>().isTrigger = true;
+            doorRenderer.enabled = false;
         }
     }
 
@@ -36,7 +48,22 @@ public class Gameplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // add "you win" screen when playerHP = 0
+        // add "you lost" screen when playerHP = 0
+        if (playerHP == 0)
+        {
+            gameOverMenu.SetActive(true);
+            lostText.SetActive(true);
+            retryText.SetActive(true);
+            timerText.SetActive(false);
+            playerHPText.text = "";
+            keyCountText.text = "";
+            Time.timeScale = 0f;
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Time.timeScale = 1f;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -55,5 +82,23 @@ public class Gameplay : MonoBehaviour
         }
 
         // add "you win" screen when player collides with house
+        if (other.gameObject.CompareTag("Door"))
+        {
+            if (keyCount == 5)
+            {
+                gameOverMenu.SetActive(true);
+                winText.SetActive(true);
+                //retryText.SetActive(true);
+                timerText.SetActive(false);
+                playerHPText.text = "";
+                keyCountText.text = "";
+                Time.timeScale = 0f;
+                //if (Input.GetKeyDown(KeyCode.R))
+                //{
+                //    Time.timeScale = 1f;
+                //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                //}
+            }
+        }
     }
 }
